65b2603e7788d81c8ea0efd612ad2cb037727675
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-08 21:15:50 +0100

    Move secrets to config

--- src/server.js
+++ src/server.js
@@ -3,17 +3,16 @@ const express    = require('express')
 const sessions   = require('client-sessions')
 const shortid    = require('shortid')
 
-const secrets = require('../secrets')
+const config  = require('../config')
+const api_url = config.gitlab_api_url
 
 const app = express()
 
-const apiUrl = 'https://gitlab2.kitnic.it/accounts/api/v4'
-
 app.use(sessions({
-  cookieName: 'session',
-  secret: secrets.cookie_secret,
-  duration: 1 * 60 * 60 * 1000,
-  activeDuration: 1000 * 60 * 5
+  cookieName     : 'session',
+  secret         : config.session_secret,
+  duration       : config.session_timeout,
+  activeDuration : config.session_timeout,
 }))
 
 app.use((req, res, next) => {
@@ -28,7 +27,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   if (req.session.token) {
-    const req_url = `${apiUrl}${req.url}`
+    const req_url = `${api_url}${req.url}`
     return superagent(req.method, req_url)
       .set('PRIVATE-TOKEN', req.session.token)
       .then(r => res.send(r.body))
@@ -39,19 +38,19 @@ app.use((req, res, next) => {
 
 function createUser() {
   const id = shortid.generate()
-  return superagent.post(`${apiUrl}/users`)
+  return superagent.post(`${api_url}/users`)
     .send({
       email         : `temp-email-for-oauth-${id}@gitlab.localhost`,
       username      : id,
       name          : id,
-      private_token : secrets.gitlab_api_token ,
+      private_token : config.gitlab_api_token ,
       password      : shortid.generate(),
     })
     .then(r => r.body)
     .then(user => {
-      return superagent.post(`${apiUrl}/users/${user.id}/impersonation_tokens`)
+      return superagent.post(`${api_url}/users/${user.id}/impersonation_tokens`)
         .send({
-          private_token : secrets.gitlab_api_token,
+          private_token : config.gitlab_api_token,
           user_id: user.id,
           name: shortid.generate(),
           scopes: ['api', 'read_user'],
