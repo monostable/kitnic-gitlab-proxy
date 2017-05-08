4af9a9d427fd3569b43001e1a1efd357faa41b1a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-08 20:30:44 +0100

    Implement access token generation

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const secrets = require('../secrets')
 const app = express()
 
 app.use(sessions({
-  cookieName: 'session', // cookie name dictates the key name added to the request object
+  cookieName: 'session',
   secret: secrets.cookie_secret,
   duration: 1 * 60 * 60 * 1000,
   activeDuration: 1000 * 60 * 5
@@ -16,11 +16,44 @@ app.use(sessions({
 
 app.use((req, res, next) => {
   if (req.session.id == null) {
-    req.session.id = shortid.generate()
-  } else {
-    console.log(req.session.id)
+    return createUser().then(info => {
+      req.session = info
+      return next()
+    })
   }
+  return next()
 })
+
+app.use((req, res, next) => {
+  res.send(req.session)
+})
+
+function createUser() {
+  const apiUrl = 'https://gitlab2.kitnic.it/accounts/api/v4/'
+  const id = shortid.generate()
+  return superagent.post(`${apiUrl}users`)
+    .send({
+      email         : `temp-email-for-oauth-${id}@gitlab.localhost`,
+      username      : id,
+      name          : id,
+      private_token : secrets.gitlab_api_token ,
+      password      : shortid.generate(),
+    })
+    .then(r => r.body)
+    .then(user => {
+      return superagent.post(`${apiUrl}users/${user.id}/impersonation_tokens`)
+        .send({
+          private_token : secrets.gitlab_api_token,
+          user_id: user.id,
+          name: shortid.generate(),
+          scopes: ['api', 'read_user'],
+        })
+        .then(r => {
+          return {id: user.id, token: r.body.token}
+        })
+    })
+}
+
 
 const port = process.env.PORT || 4003
 
